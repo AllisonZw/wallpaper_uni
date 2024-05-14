909ee2a5d3ec5de7b9b677408757cba220b784bd
dzw <1756644571@qq.com>
2024-05-14 09:00:33 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper indicator-dots indicator-color="#007aff">
+				<swiper-item v-for="item in 3">
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+ 
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.homeLayout {
+	.banner {
+		width: 750rpx;
+		padding: 30rpx 0;
+		swiper{
+			width: 750rpx;
+			height: 340rpx;
+			&-item {
+				width: 100%;
+				height: 100%;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+	}
+}
+</style>
